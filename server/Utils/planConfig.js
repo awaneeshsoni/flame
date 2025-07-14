49ad1c1a79f04c79ed41910b20b537f5ad0a6db6
Awaneesh Soni <awaneeshsoniofficial@gmail.com>
2025-07-14 08:08:40 +0530

    plans changed

--- server/Utils/planConfig.js
+++ server/Utils/planConfig.js
@@ -1,18 +1,18 @@
 const PlanConfig = {
   free: {
     maxWorkspaces: 1,
-    // storagePerWorkspace: 2 * 1024 * 1024, // 2GB
+    // storagePerWorkspace: 2 * 1024 * 1024, // 2MB
     storagePerWorkspace: 2 * 1024 * 1024 * 1024, // 2GB
-    maxMembersPerWorkspace: 2,
+    maxMembersPerWorkspace: 1,
   },
   pro: {
     maxWorkspaces: 3,
-    storagePerWorkspace: 30 * 1024 * 1024 * 1024, // 30GB
+    storagePerWorkspace: 30 * 1024 * 1024 * 1024, // total 90GB
     maxMembersPerWorkspace: 5,
   },
   business: {
-    maxWorkspaces: 10,
-    storagePerWorkspace: 100 * 1024 * 1024 * 1024, // 100GB
+    maxWorkspaces: 5,
+    storagePerWorkspace: 100 * 1024 * 1024 * 1024, // total 1000GB
     maxMembersPerWorkspace: 10,
   }
 };
